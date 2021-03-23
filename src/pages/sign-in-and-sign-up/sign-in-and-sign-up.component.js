eebe4b3e915c2ca951c9c7eef6dd38d9ac155b04
ORAQL <oscarandres-09@hotmail.com>
2021-03-23 11:55:42 -0500

    Checkpoint 6

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.style.scss';
+
+import Signin from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <Signin />
+    </div>
+)
+
+export default SignInAndSignUpPage;
