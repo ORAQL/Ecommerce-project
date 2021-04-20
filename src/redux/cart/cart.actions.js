509ca46a85e2e84c86e1116ff395d34aea7c8ea0
ORAQL <oscarandres-09@hotmail.com>
2021-04-20 13:44:10 -0500

    Project overhaul

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,9 @@ const toogleCartHidden = () => ({
     type: cartActionTypes.TOOGLE_CART_HIDDEN
 })
 
+export const addItem = item => ({
+    type: cartActionTypes.ADD_ITEM,
+    payload: item
+})
+
 export default toogleCartHidden;
